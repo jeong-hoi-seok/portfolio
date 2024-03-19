8447c2e8e61b04126bc30c311d7d0679d601f078
정회석 <jsk2343@naver.com>
2024-03-19 22:59:28 +0900

    [추가] - eslint 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "정회석 포트폴리오",
-  description: "안녕하세요. 개발자 정회석입니다!",
+    title: '정회석 포트폴리오',
+    description: '안녕하세요. 개발자 정회석입니다!',
 };
 
-const RootLayout = ({children,}: Readonly<{children: React.ReactNode;}>) => {
-  return (
-    <html lang="ko">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
+const RootLayout = ({children}: Readonly<{children: React.ReactNode;}>) => 
+{
+    return (
+        <html lang="ko">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+};
 
 export default RootLayout;
