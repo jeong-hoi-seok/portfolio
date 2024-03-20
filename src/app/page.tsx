8a37bb4ddba5c6b189b0d52e825f5f3a5b8b0022
정회석 <jsk2343@entwiz.com>
2024-03-20 15:31:23 +0900

    [수정] - 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import DustEffect from '@/components/DustEffect';
 const Home = () => 
 {
     return (
-        <div className='bg-slate-950'>
+        <div className='bg-slate-950'
+            style={{
+                background: 'linear-gradient(to top, #29323c 0%, #485563 100%)',
+            }}
+        >
             <DustEffect
                 particle={100}
                 MinSpeed={300}
                 particleMinSize={0.4}
-                particleMaxSize={0.8}
+                particleMaxSize={0.9}
             />
         </div>
     );
