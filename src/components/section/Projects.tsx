bdb771335929f4417f42e45468e22cf22d5b7d31
정회석 <jsk2343@naver.com>
2024-03-23 22:02:20 +0900

    [개발] - 프로젝트 개발 진행 중

--- src/components/section/Projects.tsx
+++ src/components/section/Projects.tsx
@@ -1,6 +1,12 @@
+'use client';
 import React from 'react';
 //
 import Container from '@/components/layout/Container';
+//swiper
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 
 const Projects = () => 
 {
@@ -9,7 +15,27 @@ const Projects = () =>
             title='프로젝트'
             subTitle='Projects'
         >
-            dddd
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={16}
+                className='mt-8'
+                navigation={true}
+                modules={[Navigation]}
+            >
+                {
+                    Array.from({length: 8}).map((_,i) => 
+                    {
+                        return (
+                            <SwiperSlide
+                                key={i}
+                                className='relative aspect-[4/3] acrylic-box'
+                            >
+                                {i + 1}
+                            </SwiperSlide>
+                        );
+                    })
+                }
+            </Swiper>
         </Container>
     );
 };
--- src/components/skill/Skills.tsx
+++ src/components/skill/Skills.tsx
@@ -12,7 +12,7 @@ import { Autoplay } from 'swiper/modules';
 const Skills = () => 
 {
     return (
-        <div className='relative h-96 rounded-lg border border-slate-50/10 bg-slate-50/5 after:absolute after:h-px after:top-0 after:w-full after:jhs-bg-gradient-to-r-glitter'>
+        <div className='relative h-96 acrylic-box'>
             <Swiper
                 autoplay={{
                     delay: 5000,
