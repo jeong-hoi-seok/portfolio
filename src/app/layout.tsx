2b1b4692daecc1358100d978b9e55954c467835a
정회석 <jsk2343@entwiz.com>
2024-03-27 12:32:56 +0900

    [테스트] - 소켓 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '../styles/globals.css';
 //component
 import Header from '@/components/common/Header';
+import SocketProvider from '@/components/provider/SocketProvider';
 
 export const metadata: Metadata = {
     title: '정회석 포트폴리오',
@@ -14,8 +15,10 @@ const RootLayout = ({children}: Readonly<{children: React.ReactNode;}>) =>
         <html lang="ko">
             <body>
                 <main>
-                    <Header/>
-                    {children}
+                    <SocketProvider>
+                        <Header/>
+                        {children}
+                    </SocketProvider>
                 </main>
             </body>
         </html>
