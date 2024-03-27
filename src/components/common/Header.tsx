ef1f2cd297a8cea274b4af4af46b563060905574
정회석 <jsk2343@naver.com>
2024-03-28 00:46:03 +0900

    [개발] - socket.io 기능 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,37 @@
+'use client';
 import React from 'react';
+import { io } from 'socket.io-client';
 
 const Header = () => 
 {
+    const [isConnect, setIsConnect] = React.useState(false);
+    const [count, setCount] = React.useState([]);
+
+    React.useEffect(() => 
+    {
+        const socket = io('ws://localhost:8080');
+
+        socket.on('connect', () => 
+        {
+            setIsConnect(true);
+            socket.on('accessor', (map) =>
+            {
+                setCount(map);
+            });
+        });
+
+        return () => 
+        {
+            socket.disconnect();
+        };
+    }, []);
+
     return (
         <header className='inline-flex fixed left-10 top-10 z-10'>
-            현재 1명이 저의 사이트를 보고있어요!
+            {
+                isConnect &&
+                `현재 ${count.length}명이 저의 사이트를 보고있어요!`
+            }
         </header>
     );
 };
