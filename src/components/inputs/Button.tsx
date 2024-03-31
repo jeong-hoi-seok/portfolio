c9f1c4c4e4c7deba646d8854a2b7285e7bea2bb3
정회석 <jsk2343@naver.com>
2024-03-31 14:14:34 +0900

    [개발] - 버튼 컴포넌트 개발

---
+++ src/components/inputs/Button.tsx
@@ -0,0 +1,140 @@
+'use client';
+import React from 'react';
+
+type IRippleProps = {
+    x: string;
+    y: string;
+}
+
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    disabled?: boolean;
+    variant?: 'text' | 'outlined' | 'contained';
+    children?: React.ReactNode;
+}
+
+const Button: React.FC<IButtonProps> = (props) => 
+{
+    const {
+        variant = 'text',
+        className,
+        children,
+        ...rest
+    } = props;
+    //ref
+    const btnDom = React.useRef(null);
+    //state
+    const [rippleColor, setRippleColor] = React.useState('#fff'); //버튼 색에 따른 ripple 색상
+    const [rippleStack, setRippleStack] = React.useState<IRippleProps[] | []>([]); //버튼 ripple stack
+
+    //클릭 트리거 확장 이벤트
+    const clickTriggerEvent = (e: React.MouseEvent<HTMLButtonElement>) => 
+    {
+        const g = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - g.left;
+        const y = e.clientY - g.top;
+
+        const ripples = {
+            x: x + 'px',
+            y: y + 'px',
+        };
+
+        setRippleStack((prev) => 
+        {
+            return [...prev, ripples];
+        });
+
+        if(props && typeof props.onClick === 'function')
+        {
+            props.onClick(e);
+        }
+    };
+
+    //버튼 레이아웃 지정
+    React.useLayoutEffect(() => 
+    {
+        const el = btnDom.current;
+        if(el)
+        {
+            const btncolor = window.getComputedStyle(el).backgroundColor;
+            const rgb = btncolor.match(/\d+/g);
+            if(rgb)
+            {
+                const brightness = (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000;
+                if(brightness > 128)
+                {
+                    setRippleColor('#535353');
+                }
+                else
+                {
+                    setRippleColor('#fff');
+                }
+            }
+        }
+    }, []);
+
+    // ripple stack 상태 업데이트
+    React.useEffect(() => 
+    {
+        if(rippleStack.length > 0)
+        {
+            const timer = setTimeout(() => 
+            {
+                setRippleStack([]);
+            }, 700);
+    
+            return () => 
+            {
+                clearTimeout(timer);
+            };
+        }
+    }, [rippleStack]);
+
+    return (
+        <button
+            ref={btnDom}
+            type={rest.type ? rest.type : 'button'}
+            {...rest}
+            onClick={clickTriggerEvent}
+            className={`
+                relative
+                inline-flex
+                justify-center
+                items-center
+                min-w-16
+                select-none
+                rounded-md
+                align-middle
+                appearance-none
+                box-border
+                no-underline
+                leading-tight
+                font-medium
+                ${variant === 'contained' ? 'bg-slate-500' : 'bg-transparent'}
+                ${variant === 'outlined' ? 'border border-slate-500' : ''}
+                ${className}
+            `}
+        >
+            {children}
+            <span className='absolute overflow-hidden pointer-events-none z-0 inset-0 rounded-[inherit]'>
+                {
+                    rippleStack.map((d,i) => 
+                    {
+                        return (
+                            <span
+                                key={i}
+                                className='absolute pointer-events-none transform -translate-x-1/2 -translate-y-1/2 rounded-full w-full aspect-square animate-button-ripple'
+                                style={{
+                                    top: d.y,
+                                    left: d.x,
+                                    backgroundColor: rippleColor,
+                                }}
+                            />
+                        );
+                    })
+                }
+            </span>
+        </button>
+    );
+};
+
+export default Button;
